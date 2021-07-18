2ad6a3eb3594c0b3c4b4d667a2bfff6b34ef9d2d
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2021-07-18 18:46:40 +0800

    chore: release

--- Formula/ripplet.rb
+++ Formula/ripplet.rb
@@ -1,12 +1,10 @@
 class Ripplet < Formula
   desc "Dynamically typed script language in Golang"
   homepage "https://github.com/hsiaosiyuan0/ripplet"
-  url "https://github.com/hsiaosiyuan0/ripplet.git", branch: "master"
+  url "https://github.com/hsiaosiyuan0/ripplet/releases/download/0.0.1/ripplet_0.0.1_darwin_amd64.tgz"
   version "0.0.1"
-  depends_on "go"
 
   def install
-    system "make", "ripplet"
-    system "make", "install"
+    bin.install "ripplet"
   end
 end
