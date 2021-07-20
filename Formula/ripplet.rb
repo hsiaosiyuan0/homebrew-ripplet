3450fe6f9dee7c17a2530080e468e301507aba5f
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2021-07-20 21:18:48 +0800

    feat: 0.0.3

--- Formula/ripplet.rb
+++ Formula/ripplet.rb
@@ -2,7 +2,7 @@ class Ripplet < Formula
   desc "Dynamically typed script language in Golang"
   homepage "https://github.com/hsiaosiyuan0/ripplet"
   url "https://github.com/hsiaosiyuan0/ripplet/releases/download/0.0.3/ripplet_0.0.3_darwin_amd64.tgz"
-  sha256 "b2ab0c104dcb92d96e0220c9115223b86efd43fb8f00624c9cfa7c5e6757c3c7"
+  sha256 "ba91e1045fdbe8917dc235ed9690a9176fc3886356ca188d63b696fd15292b69"
   version "0.0.3"
 
   def install
